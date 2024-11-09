4c1c8b2ca04f835e7a2fdf544beff9ed99a74542
Daniel Gomes Vargas <dgv@fastmail.com>
2024-11-08 21:17:32 -0300

    added fetch compiled s3db on build

--- build.zig
+++ build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const gz = std.compress.gzip;
 
 pub fn build(b: *std.Build) void {
     const use_bundled = b.option(bool, "use_bundled", "Use the bundled SQLite") orelse false;
@@ -12,6 +14,23 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .use_bundled = use_bundled,
     });
+    const s3dbfile = switch (builtin.os.tag) {
+        .linux => switch (builtin.cpu.arch) {
+            .arm => "https://github.com/jrhy/s3db/releases/download/v0.1.63/s3db-v0.1.63-linux-amd64-glibc.sqlite-ext.so.gz",
+            .aarch64 => "https://github.com/jrhy/s3db/releases/download/v0.1.63/s3db-v0.1.63-linux-amd64-glibc.sqlite-ext.so.gz",
+            .x86_64 => "https://github.com/jrhy/s3db/releases/download/v0.1.63/s3db-v0.1.63-linux-amd64-glibc.sqlite-ext.so.gz",
+            else => @compileError("arch not currently supported"),
+        },
+        .macos => switch (builtin.cpu.arch) {
+            .x86_64 => "https://pub.dgv.dev.br/s3db-v0.1.63-macos-amd64.dylib.gz",
+            else => @compileError("arch not currently supported"),
+        },
+        else => @compileError("platform not currently supported"),
+    };
+    fetch(s3dbfile) catch |err| {
+        std.debug.print("fetch err: {?}", .{err});
+        return;
+    };
     const lib = b.addStaticLibrary(.{
         .name = "s3db.zig",
         .root_source_file = b.path("src/s3db.zig"),
@@ -30,4 +49,31 @@ pub fn build(b: *std.Build) void {
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+}
+
+fn fetch(url: []const u8) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var client = std.http.Client{ .allocator = gpa.allocator() };
+    defer client.deinit();
+    var buf: [10240]u8 = undefined;
+    const uri = try std.Uri.parse(url);
+    var req = try client.open(.GET, uri, .{ .server_header_buffer = &buf });
+    defer req.deinit();
+    try req.send();
+    try req.wait();
+    const body = try req.reader().readAllAlloc(gpa.allocator(), 40000000);
+    defer gpa.allocator().free(body);
+    const filename = std.fs.path.basename(url);
+    const f = try std.fs.cwd().createFile(
+        try std.mem.concat(std.heap.page_allocator, u8, &.{ "s3db", std.fs.path.extension(filename[0 .. filename.len - std.fs.path.extension(filename).len]) }),
+        .{ .read = true },
+    );
+    defer f.close();
+    var in_stream = std.io.fixedBufferStream(body);
+    var xz_stream = std.ArrayList(u8).init(gpa.allocator());
+    defer xz_stream.deinit();
+    try gz.decompress(in_stream.reader(), xz_stream.writer());
+    try f.writeAll(xz_stream.items);
+    try f.seekTo(0);
 }
